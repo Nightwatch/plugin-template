b2ae7a25bbd577d5bab189981f9e3c52fc6357b1
Tanner Goins <tannergz28@gmail.com>
2018-05-26 15:23:23 -0500

    add prettier, semicolons, reformat

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,39 @@
-import { Message } from 'discord.js'
-import { CommandoClient } from 'discord.js-commando'
-import { Config } from '@natsuki/util'
-import { onMessage } from './lib/Events'
+import { Message } from 'discord.js';
+import { CommandoClient } from 'discord.js-commando';
+import { Config } from '@natsuki/util';
+import { onMessage } from './lib/Events';
 
 export class Module {
-  public static client: CommandoClient
-  public static config: Config
-  public static id = 'Module Template'
-  public static description = 'A template module'
+  public static client: CommandoClient;
+  public static config: Config;
+  public static id = 'Module Template';
+  public static description = 'A template module';
   public static commandGroups = [
     // If you need to make command groups for the module's commands,
     // Add them here rather than adding them to the core bot.
     // Helps keep the modules more independent from the bot.
     // ex. ['music', 'Music']
-  ]
+  ];
 
   /**
    * Initializes module
    * @param client
    * @param config
    */
-  public async init (client: CommandoClient, config: Config) {
-    Module.client = client
-    Module.config = config
-    await this.registerListeners(client, config)
+  public async init(client: CommandoClient, config: Config) {
+    Module.client = client;
+    Module.config = config;
+    await this.registerListeners(client, config);
   }
 
   /**
    * Register events
    * @param client
    */
-  private async registerListeners (client: CommandoClient, config: Config): Promise<void> {
-    client.on('message', message => onMessage(message, config))
+  private async registerListeners(
+    client: CommandoClient,
+    config: Config
+  ): Promise<void> {
+    client.on('message', (message) => onMessage(message, config));
   }
 }
--- src/lib/Events.ts
+++ src/lib/Events.ts
@@ -1,9 +1,9 @@
-import { Message } from 'discord.js'
-import axios from 'axios'
-import { Logger, Config } from '@natsuki/util'
-import { User as NatsukiUser } from '@natsuki/db'
-import { Module } from '../'
+import { Message } from 'discord.js';
+import axios from 'axios';
+import { Logger, Config } from '@natsuki/util';
+import { User as NatsukiUser } from '@natsuki/db';
+import { Module } from '../';
 
 export const onMessage = async (message: Message, config: Config) => {
   // Handle on message (if needed)
-}
+};
